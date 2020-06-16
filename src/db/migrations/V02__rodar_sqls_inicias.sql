28bbd40c861267ac71ad3126edd85200b9260aa9
Renan Luis Kist <renankist@gmail.com>
2020-06-16 10:20:38 -0300

    Update V02__rodar_sqls_inicias.sql

--- src/db/migrations/V02__rodar_sqls_inicias.sql
+++ src/db/migrations/V02__rodar_sqls_inicias.sql
@@ -1,10 +1,3 @@
--- Inserir dados na tabela 'Ação'. 
-
-insert into acao (id, nome, preco) values (1, 'MRCC', '170.20');
-insert into acao (id, nome, preco) values (2, 'KMT', '385.76');
-insert into acao (id, nome, preco) values (3, 'CART', '278.05');
-insert into acao (id, nome, preco) values (4, 'OLD', '387.30');
-
 -- Inserir dados na tabela 'Usuário'.
 insert into usuario (nome,email, senha, cpf) VALUES ('Renan', 'renankist@gmail.com', 'teste', '03248678089');
 insert into usuario (nome,email, senha, cpf) VALUES ('Morgana', 'morgana@gmail.com', 'teste', '03248678049');
