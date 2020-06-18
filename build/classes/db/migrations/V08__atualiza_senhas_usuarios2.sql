d32a5a853f877c47f0b62ec19fbf4cd469ba4b20
Renan Kist <renankist@gmail.com>
2020-06-17 23:02:19 -0300

    Tabela int3

--- build/classes/db/migrations/V08__atualiza_senhas_usuarios2.sql
+++
@@ -1,5 +0,0 @@
-create table integracao_renan (
-   id serial,
-   descricao varchar (100),
-   primary key (id));
-
--- src/db/migrations/V07__integracao.sql
+++ src/db/migrations/V07__integracao.sql
@@ -1,4 +1,4 @@
-create table integracao_renan (
+create table integracao_renan2 (
    id serial,
    descricao varchar (100),
    primary key (id)
